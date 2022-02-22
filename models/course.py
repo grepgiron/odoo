e923e8782f53e1043aaff60e60862f73952b9244
Grego <gregoriopaguada@outlook.com>
2022-02-22 09:21:40 -0600

    Create new partner categories Teacher / Level 1 and Teacher / Level 2

--- models/course.py
+++ models/course.py
@@ -15,3 +15,20 @@ class Course(models.Model):
         ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many(
         'open_academy.session', 'course_id', string="Sessions")
+
+class Session(models.Model):
+    _name = 'open_academy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date(default=fields.Date.today)
+    duration = fields.Float(digits=(6,2), help= "Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('open_academy.course',
+        ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
--- models/session.py
+++ models/session.py
@@ -5,11 +5,13 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6,2), help= "Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
 
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=[('instructor', '=', True)])
     course_id = fields.Many2one('open_academy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
