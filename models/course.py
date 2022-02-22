1d6925ed1238ae960b7dcea4cdf3a71ab1161c6d
Grego <gregoriopaguada@outlook.com>
2022-02-21 22:01:40 -0800

    Multiple Many2One relations

--- models/course.py
+++ models/course.py
@@ -13,3 +13,5 @@ class Course(models.Model):
 
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'open_academy.session', 'course_id', string="Sessions")
